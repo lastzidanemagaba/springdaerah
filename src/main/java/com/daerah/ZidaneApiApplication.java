6f8c5579e8ec111edbf5e00eef9a1d31902aa2ad
lastzidanemagaba <lastzidanemagaba@gmail.com>
2021-12-01 17:03:47 +0700

    gassss

---
+++ src/main/java/com/daerah/ZidaneApiApplication.java
@@ -0,0 +1,13 @@
+package com.daerah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZidaneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZidaneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daerah/zidaneapi/ZidaneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daerah.zidaneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZidaneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
